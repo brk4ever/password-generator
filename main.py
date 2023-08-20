fd37db88fe179108f9d5a931e6ed415a00dd604b
brk4ever <100051233+brk4ever@users.noreply.github.com>
2023-08-20 14:56:02 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+
+Random Password Generator using Python Programming Language
+Author: Burak İrtel
+'''
+import random
+import string
+
+print('This program generates random password with desired length. Password includes upper case, lower case, number, and characters!. Enjoy')
+
+length = int(input('\nEnter the length of password: '))                      
+
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+num = string.digits
+symbols = string.punctuation
+
+all = lower + upper + num + symbols
+
+temp = random.sample(all,length)
+
+password = "".join(temp)
+
+print(password)
